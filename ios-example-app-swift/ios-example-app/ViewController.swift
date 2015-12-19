66a0e5cc30696834a5d5ea5c45dc383a7b23ee4e
Jaro Habr <jaro@freshbits.ch>
2015-12-19 16:28:10 +0100

    + Update Swift example app

--- ios-example-app-swift/ios-example-app/ViewController.swift
+++ ios-example-app-swift/ios-example-app/ViewController.swift
@@ -11,15 +11,17 @@ import PathshareSDK
 
 class ViewController: UIViewController, SessionExpirationDelegate {
     
-    @IBOutlet weak var createButton: UIButton?
-    @IBOutlet weak var joinButton: UIButton?
-    @IBOutlet weak var leaveButton: UIButton?
+    let sessionIdentifierKey = "session_id"
+    var session: Session!
     
-    let session = Session.init()
+    @IBOutlet weak var createButton: UIButton!
+    @IBOutlet weak var joinButton: UIButton!
+    @IBOutlet weak var leaveButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         initButtons()
+        findSession()
     }
     
     private func initButtons() {
@@ -30,7 +32,7 @@ class ViewController: UIViewController, SessionExpirationDelegate {
     // MARK: IBActions
     
     @IBAction func createSession(sender: AnyObject) {
-        Pathshare.saveUserName("SDK User ios") { (error: NSError!) -> Void in
+        Pathshare.saveUserName("SDK User ios") { (error) -> Void in
             if error != nil {
                 NSLog("User: Error")
                 NSLog(error.description)
@@ -42,7 +44,7 @@ class ViewController: UIViewController, SessionExpirationDelegate {
     }
     
     @IBAction func joinSession(sender: AnyObject) {
-        self.session.joinUser { (error: NSError!) -> Void in
+        self.session.joinUser { (error) -> Void in
             if error != nil {
                 NSLog("Session Join: Error")
                 NSLog(error.description)
@@ -56,7 +58,7 @@ class ViewController: UIViewController, SessionExpirationDelegate {
     }
     
     @IBAction func leaveSesson(sender: AnyObject) {
-        self.session.leaveUser { (error: NSError!) -> Void in
+        self.session.leaveUser { (error) -> Void in
             if error != nil {
                 NSLog("Session Leave: Error")
                 NSLog(error.description)
@@ -64,6 +66,8 @@ class ViewController: UIViewController, SessionExpirationDelegate {
                 NSLog("Session Leave: Success")
                 self.leaveButton?.enabled = false
                 self.createButton?.enabled = true
+                
+                self.deleteSessionIdentifier()
             }
         }
     }
@@ -74,6 +78,7 @@ class ViewController: UIViewController, SessionExpirationDelegate {
         destination.latitude = 47.378178
         destination.longitude = 8.539256
         
+        self.session = Session.init()
         self.session.name = "Example Session ios"
         self.session.expirationDate = NSDate.init(timeIntervalSinceNow: 3600)
         self.session.destination = destination
@@ -88,8 +93,35 @@ class ViewController: UIViewController, SessionExpirationDelegate {
                 NSLog("Session: Success")
                 self.joinButton?.enabled = true
                 self.createButton?.enabled = false
+                
+                self.saveSessionIdentifier()
             }
         }
+    }
+    
+    private func findSession() {
+        let sessionIdentifier = NSUserDefaults.standardUserDefaults().objectForKey(sessionIdentifierKey) as? String
+        
+        guard sessionIdentifier != nil else { return }
+        
+        Pathshare .findSessionWithIdentifier(sessionIdentifier) { (session, error) -> Void in
+            if session != nil {
+                session.delegate = self
+                self.session = session
+                
+                self.createButton.enabled = false;
+                self.joinButton.enabled = true;
+                self.leaveButton.enabled = false;
+            }
+        }
+    }
+    
+    private func saveSessionIdentifier() {
+        NSUserDefaults.standardUserDefaults().setObject(self.session.identifier, forKey: sessionIdentifierKey)
+    }
+    
+    private func deleteSessionIdentifier() {
+        NSUserDefaults.standardUserDefaults().removeObjectForKey(sessionIdentifierKey)
     }
     
     // MARK: SessionExpirationDelegate
